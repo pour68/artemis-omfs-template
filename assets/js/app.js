f7e4bc19fb0ffc9275a6c4d54c40c080f89aa443
pour68 <pour68@outlook.com>
2023-07-11 21:19:31 +0330

    update clinic en

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,18 @@ quickAccessLinks.forEach(link => {
         e.currentTarget.classList.remove("deactive");
     });
 });
+
+let swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 25,
+        stretch: 0,
+        depth: 100,
+        modifier: 2,
+        slideShadows: true,
+    },
+    loop: true
+});
